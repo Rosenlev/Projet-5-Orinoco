c253a471bec4a9456f02a11ebc195653c657425e
Lucie Grand <luciemylene.grand@gmail.com>
2020-05-11 09:55:12 +0200

    add variables API url

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-// Fetch information about the teddies from API //
+//Variables
+const url= 'http://localhost:3000/api/teddies'
+
+// Fetch information about the teddies from the API //
 
 var request = new XMLHttpRequest();
 request.onreadystatechange = function() {
@@ -7,19 +10,15 @@ request.onreadystatechange = function() {
         var i;
         var i_max = response.length;
         console.log(i_max)
-        let elt = index.getElementByClass('product-filter');
         
         for (i = 0; i <= i_max; i++){
             obj = response[i]["_id"];
             console.log(obj["name"]);
             
-        elt.innerHTML = "<div class='product-info'><h6>"+obj["name"]+"</h6></div>";
         }
     }
 };
 
 request.open("GET", "http://localhost:3000/api/teddies");
 request.send();
-
-//Local storage
 
