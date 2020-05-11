8912b0f34b61cf7143953ac470118872bd6539d4
Lucie Grand <luciemylene.grand@gmail.com>
2020-05-11 10:35:37 +0200

    fetch information from the API

--- main.js
+++ main.js
@@ -2,23 +2,9 @@
 const url= 'http://localhost:3000/api/teddies'
 
 // Fetch information about the teddies from the API //
+const getTeddies = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
+}
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        var response = JSON.parse(this.responseText);
-        var i;
-        var i_max = response.length;
-        console.log(i_max)
-        
-        for (i = 0; i <= i_max; i++){
-            obj = response[i]["_id"];
-            console.log(obj["name"]);
-            
-        }
-    }
-};
-
-request.open("GET", "http://localhost:3000/api/teddies");
-request.send();
 
