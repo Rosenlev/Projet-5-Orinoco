84ee84a285bf36a8c7f8098176bf9fcbadc11250
Lucie Grand <luciemylene.grand@gmail.com>
2020-06-04 18:17:49 +0200

    comments and readme

--- product.js
+++ product.js
@@ -1,42 +1,40 @@
-// API URL
+// Variables
 const url = 'http://localhost:3000/api/teddies/';
-
-// Fetch the ID parameter from the API URL
 const parameters = new URLSearchParams(window.location.search)
-const id = parameters.get("id")
+const id = parameters.get("id") // extracts the id of the teddy we want from the URL of the page
 
-const section = document.querySelector('section'); 
+const section = document.querySelector('section'); // Selects the div in which the section will be created by JS script 
 
 
 // Fetch one teddy
 const getOneTeddy = async (productUrl, productId) => {
-    const response = await fetch(productUrl + productId);
-    return await response.json();
+    const response = await fetch(productUrl + productId); //Promise: the API URL must be retrieved
+    return await response.json(); //Parse the response
 }
 
-// Display one product
+// Display one product and the functionalities: customization option and add it to cart
 const displayProduct = async () => {
-    const data = await getOneTeddy(url, id);
-    renderTeddy(data);
-    customizeYourTeddy(section, data.colors);
-    addToCart(section, data);    
+    const data = await getOneTeddy(url, id); //Promise: the URL and the id must be retrieved to get the information from the API table
+    renderTeddy(data); // the function renderTeddy is called with the constant data as parameter
+    customizeYourTeddy(section, data.colors); // the function customizeYourTeddy is called with the constant section and data as parameters, to which is applied the colors setting
+    addToCart(section, data);    // the function addToCart is called with section and data as parameters
 }
 
 // Display product data
-function renderTeddy (productData) {
+function renderTeddy (productData) {// creates a div to be implemented into the HTML file
     section.innerHTML = `
     <div class="product-content">
         <img src="${productData.imageUrl}" alt="${productData.name}">
         <div class="product-information">
-            <h2 class="product-title">${productData.name}</h2>
-            <p class="price">${productData.price / 1000} €</p>       
+            <h1 class="product-title">${productData.name}</h1>
+            <p class="price">${productData.price / 100} €</p>       
             <p class="description">${productData.description}</p>
         </div>
     </div>`;
 }
 
 // Product customization
-function customizeYourTeddy (parentElt, productColors) {
+function customizeYourTeddy (parentElt, productColors) { // creates a dropdown menu to pick the chosen color
     // Customization Options
     const label = document.createElement('label');
     const select = document.createElement('select');
@@ -45,8 +43,8 @@ function customizeYourTeddy (parentElt, productColors) {
     label.textContent = 'Couleurs disponibles : '
     select.id = "color-list";
 
-    parentElt.appendChild(label);
-    parentElt.appendChild(select);
+    parentElt.appendChild(label); //Creates the label in HTML document
+    parentElt.appendChild(select); //Creates the dropdown menu in HTML document
 
     // Creates an option for each color thanks to the forEach() method
     productColors.forEach(productColor => {
@@ -57,8 +55,8 @@ function customizeYourTeddy (parentElt, productColors) {
     })
 
     // Fetch chosen color in the console.log
-    select.addEventListener('change', (e) => {
-        colorChosen = e.target.value;
+    select.addEventListener('change', (event) => {
+        const colorChosen = event.target.value;
         console.log(colorChosen); 
     });
 }
